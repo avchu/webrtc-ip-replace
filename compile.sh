10100caff4676a5ec141071f6141a7f312a1532c
Anton V Chunikhin <yajcms@gmail.com>
2024-08-19 10:39:42 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$JAVA_HOME/lib/server
 source /root/.sdkman/bin/sdkman-init.sh
 sdk install java 17.0.12-tem
 sdk install maven
 cd webrtc-ip-replace
-sdk use java 17.0.12-tem
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$JAVA_HOME/lib/server
+
 mvn clean install
