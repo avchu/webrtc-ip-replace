9c26fe3b7448fd943cef870be9d12f1f3d215c26
Anton V Chunikhin <yajcms@gmail.com>
2024-08-19 09:43:10 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -2,4 +2,6 @@
 source /root/.sdkman/bin/sdkman-init.sh
 sdk install java 8.0.422-amzn
 sdk install maven
-cd webrtc-ip-replace && mvn clean install
+cd webrtc-ip-replace
+sdk use java 8.0.422-amzn
+mvn clean install
